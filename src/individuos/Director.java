0b8730c348c73543ef7e1116ff88ffe5e2976748
Fabii .- <angelfcerchi@gmail.com>
2022-11-05 17:11:52 -0300

    ajustes en clases director, paciente y persona

--- src/individuos/Director.java
+++ src/individuos/Director.java
@@ -8,7 +8,8 @@ public class Director extends Persona{
         super(nombre, apellido, dni);
         
     }
-    public static Director setInstance(String nombre, String apellido, String dni, Turno[] turnos){
+
+    public static Director setInstance(String nombre, String apellido, String dni){
         if (instance == null ){
             instance = new Director(nombre, apellido, dni);
         }
