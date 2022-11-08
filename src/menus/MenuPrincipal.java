691c58d1a5639d4c0a0e1583f1c87b163720ae6f
ncoluc <nicolasmcoluccio@gmail.com>
2022-11-08 15:51:03 -0300

    Ajustes para terminar el MenuDirector y los reportes

--- src/menus/MenuPrincipal.java
+++ src/menus/MenuPrincipal.java
@@ -1,27 +1,31 @@
 package menus;
 
-import clinica.Clinica;
 import individuos.Administrativo;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MenuPrincipal {
 
-    public static void mostrarMenu(Administrativo administrativo) {
+    private final static Administrativo administrativo = new Administrativo("Fabian", "Cerchi", "37566897");
+
+    public static void mostrarMenu() {
         Scanner sn = new Scanner(System.in);
+
         boolean salir = false;
         int op;
 
         while (!salir) {
-            System.out.println("1. Menu Gerencial");
-            System.out.println("2. Menu Administrativo");
-            System.out.println("3. Menu Doctor");
-            System.out.println("4. Menu Paciente");
-            System.out.println("0. Salir");
+            MenuHelper.imprimirMenu(Arrays.asList("1. Menu Director",
+                    "2. Menu Administrativo",
+                    "3. Menu Doctor",
+                    "4. Menu Paciente",
+                    "0. Salir"));
             try {
                 op = sn.nextInt();
                 switch (op) {
                     case 1:
+                        MenuDirector.mostrarMenu(sn);
                         break;
                     case 2:
                         MenuAdministrativo.mostrarMenu(administrativo, sn);
@@ -39,6 +43,7 @@ public class MenuPrincipal {
                         break;
                     default:
                         System.out.println("Solo números entre 1 y 4. Opcion 0 para salir.");
+                        mostrarMenu();
                 }
             } catch (Exception e) {
                 System.out.println("Ocurrio un error al procesar su solicitud. Seleccione la opcion: ");
