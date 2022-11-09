3c224c58c8b071f3092c863b476d3fa571a31305
Meze <mezequielabogado@gmail.com>
2022-11-09 19:08:52 -0300

    Change assert from jupiter to junit.

--- src/test/java/clinica/ClinicaTests.java
+++ src/test/java/clinica/ClinicaTests.java
@@ -2,8 +2,9 @@ package clinica;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 public class ClinicaTests {
 
