32800cba58470a1900a01f5f1647075c705abc11
Meze <mezequielabogado@gmail.com>
2022-11-09 23:43:38 -0300

    Fix import.

--- src/test/java/clinica/ClinicaTests.java
+++ src/test/java/clinica/ClinicaTests.java
@@ -2,8 +2,7 @@ package clinica;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
 public class ClinicaTests {
