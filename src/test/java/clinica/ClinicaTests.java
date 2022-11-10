dcc63f1179e12e987691e5014421db1904ad9af0
ncoluc <nicolasmcoluccio@gmail.com>
2022-11-10 00:34:18 -0300

    se agregan test de reporte

--- src/test/java/clinica/ClinicaTests.java
+++ src/test/java/clinica/ClinicaTests.java
@@ -2,6 +2,7 @@ package clinica;
 
 import clinica.prestacion.Prestacion;
 import clinica.prestacion.PrestacionTradicional;
+import individuos.Doctor;
 import individuos.Paciente;
 import org.junit.Test;
 
@@ -44,7 +45,7 @@ public class ClinicaTests {
     }
 
     @Test
-    public void generateReportShouldWork(){
+    public void generateReportShouldWork() {
         Clinica c = Clinica.getInstance();
 
         String reporte = c.reportePrestacionesPorDoctor(c.getDoctores().get(0), false);
@@ -55,7 +56,45 @@ public class ClinicaTests {
     }
 
     @Test
-    public void agregarPrestacionShouldWork(){
+    public void generateReportWithPrestacionEstudioShouldWork() {
+        //Se selecciona arbitrariamente el primer turno disponible de una prestacion estudio
+        //y luego se hacen las asignaciones necesarias para probar el reporte de un estudio
+        Clinica c = Clinica.getInstance();
+        Doctor doctor = c.getDoctores().get(0);
+        Prestacion estudioCianuro = c.getPrestaciones().get(2);
+        estudioCianuro.setDoctorAsociado(doctor);
+        Turno turno = c.getTurnosDisponiblesPorPrestacion(estudioCianuro).get(0);
+        turno.setDoctor(doctor);
+        turno.setAsistio(true);
+        turno.setPrestacionBrindada(estudioCianuro);
+        turno.setUbicacionTurno(c.getUbicaciones().get(0));
+
+        String reporte = c.reportePrestacionesPorDoctor(doctor, true);
+        assertTrue(reporte.contains("- Analisis Cianuro - Horario: Inicio: 10/11/2022 09:00 - Fin: 10/11/2022 10:00 - Ubicacion: Consultorio 01"));
+        assertTrue(reporte.contains("El doctor Coluc Nico brindo en total 1 estudio/estudioss"));
+    }
+
+    @Test
+    public void generateReportWithPrestacionTradicionalShouldWork() {
+        //Se selecciona arbitrariamente el primer turno disponible de una prestacion tradicional
+        //y luego se hacen las asignaciones necesarias para probar el reporte de una prestacion tradicional
+        Clinica c = Clinica.getInstance();
+        Doctor doctor = c.getDoctores().get(0);
+        Prestacion prestacionTerapia = c.getPrestaciones().get(3);
+        prestacionTerapia.setDoctorAsociado(doctor);
+        Turno turno2 = c.getTurnosDisponiblesPorPrestacion(prestacionTerapia).get(0);
+        turno2.setDoctor(doctor);
+        turno2.setAsistio(true);
+        turno2.setPrestacionBrindada(prestacionTerapia);
+        turno2.setUbicacionTurno(c.getUbicaciones().get(0));
+
+        String reporte = c.reportePrestacionesPorDoctor(doctor, false);
+        assertTrue(reporte.contains("- Terapia - Horario: Inicio: 10/11/2022 09:00 - Fin: 10/11/2022 10:00 - Ubicacion: Consultorio 01"));
+        assertTrue(reporte.contains("El doctor Coluc Nico brindo en total 1 prestacion/prestaciones"));
+    }
+
+    @Test
+    public void agregarPrestacionShouldWork() {
         Clinica c = Clinica.getInstance();
         int cantidadPrestacionesOriginales = c.getPrestaciones().size();
 
@@ -64,11 +103,11 @@ public class ClinicaTests {
         c.agregarPrestacion(controlMedico);
 
 
-        assertEquals(cantidadPrestacionesOriginales+1, c.getPrestaciones().size());
+        assertEquals(cantidadPrestacionesOriginales + 1, c.getPrestaciones().size());
     }
 
     @Test
-    public void getPacientePorDNIShouldReturnResults(){
+    public void getPacientePorDNIShouldReturnResults() {
         Clinica c = Clinica.getInstance();
 
         c.getPacientes().add(new Paciente("Martin", "Abogado", "37869099", null));
@@ -80,7 +119,7 @@ public class ClinicaTests {
     }
 
     @Test
-    public void getPacientePorDNIShouldReturnNoResults(){
+    public void getPacientePorDNIShouldReturnNoResults() {
         Clinica c = Clinica.getInstance();
 
         c.getPacientes().add(new Paciente("Martin", "Abogado", "37869099", null));
@@ -91,7 +130,7 @@ public class ClinicaTests {
     }
 
     @Test
-    public void getPrestacionesPorEspecialidadShouldReturnResults(){
+    public void getPrestacionesPorEspecialidadShouldReturnResults() {
 
         Clinica c = Clinica.getInstance();
         Especialidad e = new Especialidad("Traumatologia");
@@ -104,7 +143,7 @@ public class ClinicaTests {
     }
 
     @Test
-    public void inhabilitarPrestacionShouldWork(){
+    public void inhabilitarPrestacionShouldWork() {
         Clinica c = Clinica.getInstance();
         Especialidad e = new Especialidad("Traumatologia");
         PrestacionTradicional p = new PrestacionTradicional("Control");
@@ -124,7 +163,7 @@ public class ClinicaTests {
 
 
     @Test
-    public void getListaDeEspecialidadesShouldReturnResults(){
+    public void getListaDeEspecialidadesShouldReturnResults() {
 
         Clinica c = new Clinica();
         ArrayList<Especialidad> especialidades = c.listaDeEspecialidades(true);
