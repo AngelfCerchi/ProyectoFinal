788814f8552c4316c155bc2d92d72c0c0fc8d106
angelfcerchi <Sorete2340>
2022-11-10 00:13:06 -0300

    test prescripcion

--- src/test/java/clinica/prestacion/PrescripcionTests.java
+++ src/test/java/clinica/prestacion/PrescripcionTests.java
@@ -1,2 +1,25 @@
-package clinica.prestacion;public class PrescripcionTests {
+package clinica.prestacion;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PrescripcionTests {
+
+    @Test
+    public void agregarEstudioShouldBeWork(){
+        Estudio estudio = new Estudio("Orina Completa");
+        Prescripcion prescripcion = new Prescripcion();
+        prescripcion.agregarEstudio(estudio);
+        assertEquals(1,prescripcion.getEstudios().size());
+    }
+
+    @Test
+    public void agregarMedicamentoShouldBeWork(){
+        Medicamento medicamento = new Medicamento("Paracetamol",1);
+        Prescripcion prescripcion = new Prescripcion();
+        prescripcion.agregarMedicamento(medicamento);
+        assertEquals(1,prescripcion.getMedicamentos().size());
+    }
 }
